1e70cf6702cbc5fbc9914beec403228c9d2757aa
Beni Shamavu <d00253976@student.dkit.ie>
2025-04-29 02:04:33 +0100

    Fixed deployment issues on Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -6,38 +6,38 @@ import Image from 'next/image';
 
 export default function Page() {
   return (
+    // ...
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-      <AcmeLogo/>
-        {/* <AcmeLogo /> */}
+    <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <p
+          <p
       className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
     >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
+      <strong>Welcome to Dundalk Code Club.</strong> Click the blue text to be directed to the dashboard home page{' '}
+      <a href="/dashboard" className="text-blue-500">
+        dashboard home page
+      </a>
+      , Author: Beni Shamavu, using Vercel nextjs framework.
+    </p>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-          <Image
-        src="/hero-desktop.png"
+      {/* Add Hero Images Here */}
+      <Image
+        src="/web.png"
         width={1000}
         height={760}
         className="hidden md:block"
         alt="Screenshots of the dashboard project showing desktop version"
       />
+      <Image
+      src="/python.png"
+      width={560}
+      height={620}
+      className="block md:hidden"
+      alt="Screenshots of the dashboard project showing mobile version (layout example on mobile device))"/>
         </div>
       </div>
     </main>
