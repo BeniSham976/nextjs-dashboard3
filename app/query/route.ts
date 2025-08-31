5221612ef8261f9b2883ffa7ed314f750f2b6276
Beni Shamavu <d00253976@student.dkit.ie>
2025-08-31 16:26:05 +0100

    uncommenting full file

--- app/query/route.ts
+++ app/query/route.ts
@@ -14,9 +14,9 @@ import postgres from 'postgres';
  }
 
 export async function GET() {
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+   try {
+   	return Response.json(await listInvoices());
+   } catch (error) {
+   	return Response.json({ error }, { status: 500 });
+   }
 }
